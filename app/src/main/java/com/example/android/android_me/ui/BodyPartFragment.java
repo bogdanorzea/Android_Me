dcfcf6af351121cb284789adfe53e4ee5ac52561
cezannec <cezanne.camacho@gmail.com>
2017-02-27 07:44:45 +0200

    TFragments.03-Exercise-ClicksAndSavingState
    (cherry picked from commit c245127)

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,3 +1,19 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
@@ -14,17 +30,14 @@ import java.util.List;
 
 public class BodyPartFragment extends Fragment {
 
-    private static final String LOG_TAG = BodyPartFragment.class.getSimpleName();
+    // TODO (3) Create final Strings to store state information about the list of images and list index
+
+    // Tag for logging
+    private static final String TAG = "BodyPartFragment";
+
+    // Variables to store a list of image resources and the index of the image that this fragment displays
     private List<Integer> mImageIds;
     private int mListIndex;
-
-    public void setImageResources(List<Integer> imageIdList) {
-        this.mImageIds = imageIdList;
-    }
-
-    public void setIndex(int index) {
-        this.mListIndex = index;
-    }
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the fragment
@@ -44,14 +57,33 @@ public class BodyPartFragment extends Fragment {
         // Get a reference to the ImageView in the fragment layout
         ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
+        // If a list of image ids exists, set the image resource to the correct item in that list
+        // Otherwise, create a Log statement that indicates that the list was not found
         if(mImageIds != null){
+            // Set the image resource to the list item at the stored index
             imageView.setImageResource(mImageIds.get(mListIndex));
+
+            // TODO (1) Set a click listener on the image view and on a click increment the list index and set the image resource
+            // TODO (2) If you reach the end of a list of images, set the list index back to 0 (the first item in the list)
+
         } else {
-            Log.e(LOG_TAG, "Invalid ImageResources list");
+            Log.v(TAG, "This fragment has a null list of image id's");
         }
 
         // Return the rootView
         return rootView;
     }
 
+    // Setter methods for keeping track of the list images this fragment can display and which image
+    // in the list is currently being displayed
+
+    public void setImageIds(List<Integer> imageIds) {
+        mImageIds = imageIds;
+    }
+
+    public void setListIndex(int index) {
+        mListIndex = index;
+    }
+
+    // TODO (4) Override onSaveInstanceState and save the current state of this fragment
 }
